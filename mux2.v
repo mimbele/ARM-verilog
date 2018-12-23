aa085a62ee2ba6b2ff1908e5832ddcf3efd3f2ce
mimbele <mrym.ndr@gmail.com>
2018-12-23 15:08:49 +0330

    Renamed Sel to Select

--- mux2.v
+++ mux2.v
@@ -1,9 +1,9 @@
-module mux2(A, B, Sel, Out);
+module mux2(A, B, Select, Out);
 	parameter n=64;
 	input [n-1:0] A;
 	input [n-1:0] B;
-	input Sel;
+	input Select;
 	output [n-1:0] Out;
 	
-	assign Out = Sel ? B : A;
+	assign Out = Select ? B : A;
 endmodule
