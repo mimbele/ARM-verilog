fe034cd0728d53838fa0bff448630f59d82d4400
Hamidreza <h.kalantari.1997@gmail.com>
2018-12-27 14:02:25 +0700

    typo

--- sign_extend.v
+++ sign_extend.v
@@ -4,5 +4,5 @@ module sign_extend(Instruction, Out);
 
 	wire Sign;
 	assign Sign = Instruction[30] ? Instruction[20] : Instruction[23];
-	assign Out = Instruction[30] ? ({{55{Sign}}, Instruction[20:12]}) : ({{45{Sign}}, Instruction[23:5]}));	
+	assign Out = Instruction[30] ? ({{55{Sign}}, Instruction[20:12]}) : ({{45{Sign}}, Instruction[23:5]});	
 endmodule
