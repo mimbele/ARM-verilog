ebaf051f72253705480512d2de8499e356c8472a
Hamidreza <h.kalantari.1997@gmail.com>
2018-12-27 14:02:52 +0700

    Initialize register bank with register value = register address

--- register_bank.v
+++ register_bank.v
@@ -9,10 +9,17 @@ module register_bank(clk, read_register1, read_register2, write_register, write_
 	output [63:0] read_data2;
 	
 	reg [63:0] registers [0:31];
-	
-	
+
 	assign read_data1 = registers[read_register1];
 	assign read_data2 = registers[read_register2];
+
+	integer i;
+	initial begin: initial_reset
+
+		for(i = 0; i < 32; i = i + 1) begin: initial_reset_for
+			registers[i] = i;
+		end
+	end
 	
 	always @(posedge clk)begin
 		if(write==1'b1)begin
